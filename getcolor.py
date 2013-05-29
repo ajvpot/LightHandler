832e42d3a77f63a5464638dc2b843d917d4114cb
ajvpot <ajvpot>
2013-05-29 17:01:41 -0500

    Rename stuff for consistency

--- getcolor.py
+++ getcolor.py
@@ -2,6 +2,6 @@ from twisted.web import resource
 
 class GetColorResource(resource.Resource):
     def __init__(self, queue):
-        self.queue = queue
+        self.interface = interface
     def render_GET(self, request):
-        return "%s" % self.queue.current
+        return "%s" % self.interface.current
--- setintensity.py
+++ setintensity.py
@@ -1,8 +1,8 @@
 from twisted.web import resource
 
 class SetIntensityResource(resource.Resource):
-    def __init__(self, queue):
-        self.queue = queue
+    def __init__(self, interface):
+        self.interface = interface
     def render_GET(self, request):
-        self.queue.max = int(request.args["intensity"][0])/float(100)
-        return "Set intensity to %s" % self.queue.max
+        self.interface.max = int(request.args["intensity"][0])/float(100)
+        return "Set intensity to %s" % self.interface.max
